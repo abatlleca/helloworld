6dd1dc354c059a78b5a4146975d1727f479660d4
abatlleca <abatlleca@gmail.com>
2020-06-19 12:28:17 +0200

    edit index and readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-"# helloworld" 
+# Projecte helloworld multi-idioma
+
+## Instal·lació en Windows:
+Per instal·lar l'aplicació en un entorn Windows, és necessari instal·lar la següent llista d'aplicacions:
+1. Paquet ![Image XAMPP](https://images.app.goo.gl/puZDfY4gYzVRfqKW9) XAMPP Apache + MariaDB + PHP + Perl
+- An Apache + PHP + Mysql environment is needed (or similar)
+- Install last Composer version https://getcomposer.org/download/ 
+- Install NodeJS from https://nodejs.org/
+- Create a new database with utf8mb4_unicode_ci 
+- Create a user with permissions to interact with the new database
+- Download or Clone the Git repository https://github.com/abatlleca/LaravelCoreApp
+- Copy and rename `.env.example` file to `.env` inside your project root 
+- Fill the database information inside `.env`
+- Open the console and cd your project root directory
+- Run `composer install` or ```php composer.phar install``` to install the `vendor` files
+- Run `php artisan key:generate` to create the secret key for the project
+- Run `php artisan migrate` to create the tables
+- Run `php artisan db:seed` to run seeders, if any
+- Run `npm run dev` to create the public JS and CSS files
+- Run `php artisan serve` to serve the project to Apache (default port 8000). Optional add `--port=8080`
+- Open `httpd-vhosts.conf` file of Apache → <apache_folder>\conf\extra
+- Add VirtualHost for the project
+
+  Ex.:
+  ```
+  <VirtualHost <desiredURL>:80>
+  	DocumentRoot "<laravel_project_folder>/public"
+  	<Directory "<laravel_project_folder>/public">
+  		Options Indexes FollowSymLinks
+  		AllowOverride All
+  		Require all granted
+  	</Directory>
+  </VirtualHost>
+  ```
+  Open `hosts` file
+   
+  (In Windows) C:\Windows\system32\drivers\etc\
+  (Linux and Mac OS) /etc/hosts
+  
+  Add new line `127.0.0.1 <desiredURL>`
+
+##### You can now access your project at `<desiredURL>:8000` 
--- index.php
+++ index.php
@@ -9,12 +9,71 @@
     $i18n->init();
 ?>
 
-<!-- get applied language -->
-<p>Applied Language: <?php echo $i18n->getAppliedLang(); ?> </p>
+<html>
+<head>
+<title><?php echo L::greeting; ?></title>    
 
-<!-- get the cache path -->
-<p>Cache path: <?php echo $i18n->getCachePath(); ?></p>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-<!-- Get some greetings -->
-<p>A greeting: <?php echo L::greeting; ?></p>
-<p>Something other: <?php echo L::category_somethingother; ?></p><!-- normally sections in the ini are seperated with an underscore like here. -->
+    <!-- Styles -->
+    <link href="assets/css/app.css" rel="stylesheet">
+</head>
+
+<body>
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="/">
+                <?php echo L::greeting; ?>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Language Links -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="/?lang=ca">CA</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/?lang=es">ES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/?lang=en">EN</a>
+                        </li>
+                    </ul>
+                    
+            </div>
+        </div>
+    </nav>
+</div>
+<main class="py-4">
+    <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header"></div>
+
+                        <div class="card-body">
+                            <!-- Get some greetings -->
+                            <p><?php echo L::greeting; ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</main>
+</body>
+</html>
+
+
